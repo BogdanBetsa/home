7f8e393d0ab6f04e7e77bf86b5c8b50dc27aa844
BogdanBetsa <bogdanbeca1991@gmail.com>
2017-05-26 21:06:41 +0300

    two commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 //catch(e){
 //    console.warn(e.message);
 //}
-
+//
 //(function(){
 //    var x = function(select){
 //        console.log(select);
@@ -35,8 +35,90 @@
 //    
 //},1000);
 //clearInterval(a);
-
+//
 //var a = setTimeout(function(){
 //            console.log(new Date())
 //    
 //},1000);
+//
+//
+//console.log(ex); // вызов по id
+//var a = document.getElementsByClassName('className') // вызов по class
+//console.log(a[0]);
+//document.getElementsByName('div'); // вызов по tag
+//console.log(a);
+//
+//var ex = document.getElementById('ex');
+//ex.style.height = "200px";
+//ex.style.width = "500px";
+//ex.style.backgroundColor = 'red';
+//ex.style.color = 'white';
+//console.log(ex);
+//
+//var btn = document.getElementById('btn');
+//btn.onclick = function(){
+//    ex.style.backgroundColor = 'green';
+//}
+//
+//btn.onclick = function(){
+//    var username = document.getElementById('username').value;
+//    ex.innerHTML = 'Hello' + username;
+//  
+//}
+//btn.click = function(){
+//    var username = document.getElementById('username').value;
+//    var password = document.getElementById('password').value;
+//    if(username == 'vasya' && password == '123'){
+//        ex.innerHTML = 'ok';
+//    }else{
+//        ex.innerHTML = 'fail';
+//    }
+//    }
+
+
+function BombKiller(n) {
+    function randomize(){
+        return Math.floor(Math.random() * (n - 1)) + 1;
+    }
+
+this.start = function () {
+    var code = randomize();
+    var sector = document.getElementsByClassName('sector');
+    sector[--code].nobomb = 1;
+    for (var i = 0; i < sector.length; i++) {
+        sector[i].onclick = function () {
+            var z = i;
+            if (this.nobomb == 1) {
+                this.style.backgroundColor = 'green';
+            }else {
+                    this.style.backgroundColor = 'red';
+
+                }
+            }
+        }
+    }
+}
+
+
+var Bomb = new BombKiller(4);
+Bomb.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
